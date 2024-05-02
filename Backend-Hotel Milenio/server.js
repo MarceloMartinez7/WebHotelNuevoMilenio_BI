50a0e1d4ccb756b2e08ae660a1f4351c136721ab
MarceloMartinez7 <mr5405750@gmail.com>
2024-05-02 09:36:57 -0600

    Cambios xd

--- Backend-Hotel Milenio/server.js
+++ Backend-Hotel Milenio/server.js
@@ -11,7 +11,7 @@ app.use(express.json({limit: '50mb'}))
 const db = mysql.createConnection({
   host: 'localhost',
   user: 'root',
-  password: 'Yamilg620',
+  password: 'Marcelox2021',
   database: 'db_hotelmilenio',
 });
 
