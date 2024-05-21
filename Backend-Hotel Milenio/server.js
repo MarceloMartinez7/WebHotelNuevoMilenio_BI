018527b017c93a88b3917f52b539a9ba13b724fb
Daniela Baltodano <alejandrabaltodano67@gmail.com>
2024-05-21 08:33:08 -0600

    Xd

--- Backend-Hotel Milenio/server.js
+++ Backend-Hotel Milenio/server.js
@@ -11,7 +11,7 @@ app.use(express.json({limit: '50mb'}))
 const db = mysql.createConnection({
   host: 'localhost',
   user: 'root',
-  password: 'Marcelox2021',
+  password: 'Mysql2023',
   database: 'db_hotelmilenio',
 });
 
